082b2d3054b246fcd7fe8598ec952a9b50c581b6
baderbenlhachemi <baderbenlhachemi@gmail.com>
2024-02-26 13:41:17 +0100

    Configure REST APIs to readonly

--- backend/springboot-ecommerce/src/main/java/com/example/springbootecommerce/dao/ProductCategoryRepository.java
+++ backend/springboot-ecommerce/src/main/java/com/example/springbootecommerce/dao/ProductCategoryRepository.java
@@ -3,7 +3,9 @@ package com.example.springbootecommerce.dao;
 import com.example.springbootecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
+@Repository
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
